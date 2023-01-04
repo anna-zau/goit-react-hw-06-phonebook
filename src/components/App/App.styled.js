f70f5162dac740215527b7e3d99c7ce13dec3687
anna-zau <annazauholnikova555@gmail.com>
2023-01-04 23:34:51 +0200

    commit

---
+++ src/components/App/App.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const DivSection = styled.div`
+  display: block;
+  margin: 0 auto;
+  padding: 30px 30px;
+  width: 320px;
+  border: 2px solid #e7dae8;
+  border-radius: 15px;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-top: 0;
+`;
+
+export const TitleTwo = styled.h2`
+  text-align: center;
+`;
